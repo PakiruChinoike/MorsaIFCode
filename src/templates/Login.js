86f0db5d2956662894b731ebd546e7ea83a5eaab
Hertzog <eric.hertzog@sap.com>
2025-09-13 04:46:10 -0300

    meio login

--- src/templates/Login.js
+++ src/templates/Login.js
@@ -15,13 +15,15 @@ function Login() {
 
   return (
     <div className="login-container">
-      <h2>Login</h2>
+      <img src="https://static.vecteezy.com/system/resources/previews/029/338/059/non_2x/3d-home-sweet-home-and-pink-hearts-on-a-transparent-background-free-png.png" alt="Logo" className="login-logo" />
+      <h2 className="login-title">Bem-Vindo(a)!</h2>
       <FacebookSDKLoader />
-      <button onClick={handleFacebookLogin}>Login with Facebook</button>
+      <button onClick={handleFacebookLogin} className="login-button facebook-button">Login with Facebook</button>
       <button onClick={handleGoogleLogin} className="login-button google-button">
         Login with Google
       </button>
-      <button onClick={handleLogin}>Login</button>
+      <div>_______________</div>
+      <button onClick={handleLogin} className="login-button">Login</button>
     </div>);
 }
 
