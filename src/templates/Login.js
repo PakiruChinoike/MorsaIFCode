90968c44ac24e4e707428a98f044a6a63c007fb2
maridagomes <mariagomes.ss007@academico.ifsul.edu.br>
2025-09-13 04:28:57 -0300

    login

--- src/templates/Login.js
+++ src/templates/Login.js
@@ -3,25 +3,43 @@ import { useNavigate } from 'react-router-dom';
 import '../static/style.css';
 import { handleFacebookLogin, FacebookSDKLoader } from '../components/FacebookSSO';
 import handleGoogleLogin from '../components/GoogleSSO';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
   const { login } = useAuth();
   const navigate = useNavigate();
+  const [loginEmail, setLoginEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [user, setUser] = useState("")
 
   const handleLogin = () => {
-    login();
+    axios.get('')
+        .then(response => {
+            console.log(response.data)
+            setUser(response.data)
+        })
+        .catch(error => {
+            console.error(error)
+        });
+
+    login(user);
     navigate('/home');
   };
 
   return (
     <div className="login-container">
       <h2>Login</h2>
+      <label for='login'>E-mail:</label>
+      <input type='text' name='login' value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)}/>
+      <label for='senha'>Senha:</label>
+      <input type='text' name='senha' value={senha} onChange={(e) => setSenha(e.target.value)}/>
+      <button onClick={handleLogin}>Login</button>
       <FacebookSDKLoader />
       <button onClick={handleFacebookLogin}>Login with Facebook</button>
       <button onClick={handleGoogleLogin} className="login-button google-button">
         Login with Google
       </button>
-      <button onClick={handleLogin}>Login</button>
     </div>);
 }
 
