95e38bff592670c2e23fa13825cb609fb8a4fc4f
maridagomes <mariagomes.ss007@academico.ifsul.edu.br>
2025-09-13 03:25:36 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './components/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+
+          <Route path='/' element={<ProtectedRoute element={<Home />} />} />
+          <Route path='/home' element={<ProtectedRoute element={<Home />} />} />
+          <Route path='/perfil' element={<ProtectedRoute element={<Perfil />} />} />
+          <Route path='/interesses' element={<ProtectedRoute element={<Interesses />} />} />
+          <Route path='/criar-post' element={<ProtectedRoute element={<CriarPost />} />} />
+          <Route path='/criar-conta' element={<ProtectedRoute element={<CriarConta />} />} />
+          <Route path='/comunidade' element={<ProtectedRoute element={<Comunidade />} />} />
+          <Route path='/chat-comunidade' element={<ProtectedRoute element={<ChatComunidade />} />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/templates/Login.js
+++ src/templates/Login.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
+  const handleLogin = () => {
+    login();
+    navigate('/home'); 
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 }
 
 export default Login;
